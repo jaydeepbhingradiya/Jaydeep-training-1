1c6122bc2e330018f9a5169a0f5ca6aee9d6fca6
DESKTOP-BNEE4AU\Jaydeep <jaydeepb.albiorix@gmail.com>
2022-01-10 19:00:43 +0530

    REACT JS : react-hooks re study : 10-01-2022

--- REACT JS/13-hooks-update/src/components/Ingredients/IngredientForm.js
+++ REACT JS/13-hooks-update/src/components/Ingredients/IngredientForm.js
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Card from '../UI/Card';
-import './IngredientForm.css';
+import Card from "../UI/Card";
+import "./IngredientForm.css";
 
-const IngredientForm = React.memo(props => {
-  const [enteredTitle, setEnteredTitle] = useState('');
-  const [enteredAmount, setEnteredAmount] = useState('');
+const IngredientForm = React.memo((props) => {
+  const { onAddIngredient } = props;
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
 
-  const submitHandler = event => {
+  const submitHandler = (event) => {
     event.preventDefault();
-    props.onAddIngredient({ title: enteredTitle, amount: enteredAmount });
+    onAddIngredient({ title: enteredTitle, amount: enteredAmount });
   };
 
   return (
@@ -22,7 +23,7 @@ const IngredientForm = React.memo(props => {
               type="text"
               id="title"
               value={enteredTitle}
-              onChange={event => {
+              onChange={(event) => {
                 setEnteredTitle(event.target.value);
               }}
             />
@@ -33,7 +34,7 @@ const IngredientForm = React.memo(props => {
               type="number"
               id="amount"
               value={enteredAmount}
-              onChange={event => {
+              onChange={(event) => {
                 setEnteredAmount(event.target.value);
               }}
             />
--- REACT JS/13-hooks-update/src/components/UI/ErrorModal.js
+++ REACT JS/13-hooks-update/src/components/UI/ErrorModal.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import './ErrorModal.css';
+import "./ErrorModal.css";
 
-const ErrorModal = React.memo(props => {
+const ErrorModal = React.memo((props) => {
+  const { onClose, children } = props;
   return (
     <React.Fragment>
-      <div className="backdrop" onClick={props.onClose} />
+      <div className="backdrop" onClick={onClose} />
       <div className="error-modal">
         <h2>An Error Occurred!</h2>
-        <p>{props.children}</p>
+        <p>{children}</p>
         <div className="error-modal__actions">
-          <button type="button" onClick={props.onClose}>
+          <button type="button" onClick={onClose}>
             Okay
           </button>
         </div>
