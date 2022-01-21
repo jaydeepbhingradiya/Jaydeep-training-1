a29fc65144b7ff6955c9d37888d2dd0379ee09d6
DESKTOP-BNEE4AU\Jaydeep <jaydeepb.albiorix@gmail.com>
2022-01-21 18:09:45 +0530

    react js : 21-01-2022

--- stepper-demo/src/components/deatails/PersonalDetails.js
+++ stepper-demo/src/components/deatails/PersonalDetails.js
@@ -1,40 +1,56 @@
-import React from "react";
-import classes from "./PersonalDetails.module.css";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 function PersonalDetails() {
   return (
-    <div>
-      <div className="App">
-        <h2>Personal Details</h2>
+    <Box
+      component="form"
+      sx={{
+        "& .MuiTextField-root": { m: 1, width: "25ch" },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <div className="App">
+          <h2>Personal Details</h2>
+        </div>
+        <TextField error id="outlined-error" label="Error" />
+        <TextField
+          error
+          id="outlined-error-helper-text"
+          label="Error"
+          helperText="Incorrect entry."
+        />
       </div>
       <div>
-        <section className={classes.auth}>
-          <form>
-            <div className={classes.control}>
-              <label htmlFor="firstName">First Name</label>
-              <input type="text" id="firstName" required />
-            </div>
-            <div className={classes.control}>
-              <label htmlFor="lastName">Last Name</label>
-              <input type="text" id="lastName" required />
-            </div>
-            <div className={classes.control}>
-              <label htmlFor="email">Your Email</label>
-              <input type="email" id="email" required />
-            </div>
-            <div className={classes.control}>
-              <label htmlFor="phone">Phone Number</label>
-              <input type="tel" id="phone" required />
-            </div>
-            <div className={classes.control}>
-              <label htmlFor="dateOfBirth">Phone Number</label>
-              <input type="date" id="dateOfBirth" required />
-            </div>
-          </form>
-        </section>
+        <TextField error id="filled-error" label="Error" variant="filled" />
+        <TextField
+          error
+          id="filled-error-helper-text"
+          label="Error"
+          helperText="Incorrect entry."
+          variant="filled"
+        />
       </div>
-    </div>
+      <div>
+        <TextField
+          error
+          id="standard-error"
+          label="Error"
+          defaultValue="Hello World"
+          variant="standard"
+        />
+        <TextField
+          error
+          id="standard-error-helper-text"
+          label="Error"
+          helperText="Incorrect entry."
+          variant="standard"
+        />
+      </div>
+    </Box>
   );
 }
-
 export default PersonalDetails;
